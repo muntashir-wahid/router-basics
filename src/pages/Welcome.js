5a0a8c9e573decdccff4500b2c191abcf2971c95
muntashir-wahid <muntashir.wahid@yahoo.com>
2022-10-06 23:05:26 +0600

    learnt nested routing

--- src/pages/Welcome.js
+++ src/pages/Welcome.js
@@ -1,7 +1,15 @@
 import React from "react";
+import { Route } from "react-router-dom";
 
 const Welcome = () => {
-  return <h1>The Welcome Page!</h1>;
+  return (
+    <section>
+      <h1>The Welcome Page!</h1>
+      <Route path="/welcome/new-user">
+        <h2>Hello New User</h2>
+      </Route>
+    </section>
+  );
 };
 
 export default Welcome;
